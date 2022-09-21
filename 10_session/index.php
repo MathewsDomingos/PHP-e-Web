50fc2245dbac05efbc8d129f72c54383eb6971a9
Mateus Domingos Felipe <mateusdomingos.etec@gmail.com>
2022-09-20 22:08:40 -0300

    Sessions

---
+++ 10_session/index.php
@@ -0,0 +1,18 @@
+<?php
+  session_start();
+  $_SESSION['nome'] = 'Mateus';
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Sessiont</title>
+</head>
+<body>
+<h1>Olá mundo</h1>
+</body>
+</html>
---
+++ 10_session/pagina.php
@@ -0,0 +1,18 @@
+<?php
+  session_start();
+  print_r($_SESSION);
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Sessiont</title>
+</head>
+<body>
+<h1>Página</h1>
+</body>
+</html>
